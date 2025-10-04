ab79724ee9b879077ef4aca23ca74cd238f139ad
BartoszRP <reszka.bartosz@gmail.com>
2025-10-04 18:22:38 +0200

    Flask app finished

--- Chatbot_web_version/app.py
+++ Chatbot_web_version/app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Wczytanie klucza API z pliku .env
+load_dotenv()
+client = OpenAI()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/chat/hello")
 def home():
     return render_template("index.html")
+
+def send_message(message, previous_response_id):
+
+    response = client.responses.create(
+        model ="gpt-4.1-mini",
+        input = message,
+        previous_response_id = previous_response_id
+    )
+    return response.output_text, response.id
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+
+    if not data or "message" not in data:
+        return jsonify({"error": "No message provided"}), 400
+
+    message = data["message"]
+    previous_response_id = data.get("previous_response_id")
+
+    response, response_id=send_message(message, previous_response_id)
+
+    return jsonify({
+        "response": response,
+        "response_id": response_id
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
